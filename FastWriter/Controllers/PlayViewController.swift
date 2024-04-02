6ce0fd59b5b80ca7fdbf4d6a89ff5ea139664c73
alvinjhson <alvinbest26@gmail.com>
2024-04-02 09:22:26 +0200

    You can now write in words

--- FastWriter/Controllers/PlayViewController.swift
+++ FastWriter/Controllers/PlayViewController.swift
@@ -7,17 +7,54 @@
 
 import UIKit
 
-class PlayViewController: UIViewController {
+class PlayViewController: UIViewController, UITextFieldDelegate  {
+    
+    var word = Word()
 
+    @IBOutlet weak var answerWord: UITextField!
+    @IBOutlet weak var currentWord: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        answerWord.delegate = self
+        answerWord.becomeFirstResponder()
+        randomWord()
+    
+        
+    }
+  
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.text == currentWord.text {
+            randomWord()
+            textField.text = ""
+        }
+        return true
+        
     }
     
+    func randomWord() {
+        if let randomWord = word.getRandomWord() {
+            currentWord.text = randomWord.word
+    
+        } else {
+            currentWord.text = "Oops! Inga fler ord!"
+        }
+    }
+   // override func viewDidAppear(_ animated: Bool) {
+     //   super.viewDidAppear(animated)
+     //   answerWord.becomeFirstResponder()
+   // }
+        
+        
+        
+    
+        
 
+
+    
+  
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
--- FastWriter/Models/Word.swift
+++ FastWriter/Models/Word.swift
@@ -7,7 +7,34 @@
 
 import Foundation
 
-
-struct Word {
+struct structWord {
     var word: String
 }
+
+class Word {
+    var words = [structWord]()
+    
+    init() {
+        words.append(structWord(word: "Hej"))
+        words.append(structWord(word: "Bil"))
+        words.append(structWord(word: "Hus"))
+        words.append(structWord(word: "Sol"))
+        words.append(structWord(word: "Hund"))
+        words.append(structWord(word: "Träd"))
+        words.append(structWord(word: "Mat"))
+        words.append(structWord(word: "Vatten"))
+        words.append(structWord(word: "Cykel"))
+        words.append(structWord(word: "Bok"))
+        words.append(structWord(word: "Traktor"))
+    }
+    
+    func getRandomWord() -> structWord? {
+        if !words.isEmpty {
+            return words.randomElement()
+        } else {
+            return nil
+        }
+    }
+}
+
+
