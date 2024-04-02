8450e5e08585edf5d8a08b48c6d02194e11a6334
Jonas Bondesson <jonasbondesson@yahoo.com>
2024-04-02 11:31:58 +0200

    fixed gamemode

--- FastWriter/Models/User.swift
+++ FastWriter/Models/User.swift
@@ -7,10 +7,29 @@
 
 import Foundation
 
-struct User {
+class User {
     var name: String
     var highscore: Int
-    var timeLeft: Double
+    private var timeLeft: Double
+    private var score: Int
+    
+    
+
+    
+    init(name: String, highscore: Int, timeLeft: Double, score: Int) {
+        self.name = name
+        self.highscore = highscore
+        self.timeLeft = timeLeft
+        self.score = score
+    }
+    
+    func setScore(score: Int) {
+        self.score += score
+    }
+    
+    var getScore: Int {return self.score}
+    
+    
 }
 
 func timeLeft(timer: Timer?) {
