9b1261c8c20eb780b778f1cbf6e4bfc3545fb135
Jonas Bondesson <jonasbondesson@yahoo.com>
2024-04-02 09:48:09 +0200

    added label

--- FastWriter/Controllers/PlayViewController.swift
+++ FastWriter/Controllers/PlayViewController.swift
@@ -9,19 +9,38 @@ import UIKit
 
 class PlayViewController: UIViewController, UITextFieldDelegate  {
     
+    var user = User(name: "Jonas", highscore: 0, timeLeft: 30.0)
+    
     var word = Word()
+    var time = TimeInterval(30)
+    var timer: Timer?
+    let formatter = DateComponentsFormatter()
 
+    @IBOutlet weak var timeLeft: UILabel!
     @IBOutlet weak var answerWord: UITextField!
     @IBOutlet weak var currentWord: UILabel!
+    @IBOutlet weak var score: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         answerWord.delegate = self
         answerWord.becomeFirstResponder()
+        
         randomWord()
     
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: changeTime(timer:))
         
     }
-  
+    
+    func changeTime(timer: Timer?) {
+        if time > 0 {
+            time -= 1
+            timeLeft.text = formatter.string(from: time)
+        } else {
+            
+        }
+        
+    }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField.text == currentWord.text {
@@ -34,12 +53,15 @@ class PlayViewController: UIViewController, UITextFieldDelegate  {
     
     func randomWord() {
         if let randomWord = word.getRandomWord() {
+            user.highscore += 10
             currentWord.text = randomWord.word
     
         } else {
             currentWord.text = "Oops! Inga fler ord!"
         }
     }
+    
+    
    // override func viewDidAppear(_ animated: Bool) {
      //   super.viewDidAppear(animated)
      //   answerWord.becomeFirstResponder()
